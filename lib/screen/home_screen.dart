0d48d52fa2bf933d37be9488dc197af0acfb9d3c
bam-e22 <stack.07142@gmail.com>
2023-03-31 15:59:38 +0900

    appbar

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        title: Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
