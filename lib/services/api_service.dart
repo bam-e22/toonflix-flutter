ad7f5f484ab6fb9fa2306ffa5f295add126b4562
bam-e22 <stack.07142@gmail.com>
2023-04-02 14:56:28 +0900

    fromJson

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toontoonflix/model/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      return jsonDecode(response.body)
+          .map((json) => WebtoonModel.fromJson(json))
+          .toList();
     }
     throw Error();
   }
