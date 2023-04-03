483721bb8fe6f6313db3ac40e8e0a63800cec569
bam-e22 <stack.07142@gmail.com>
2023-04-03 17:42:12 +0900

    hero

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -23,26 +23,29 @@ class Webtoon extends StatelessWidget {
                       thumb: thumb,
                       id: id,
                     ),
-                fullscreenDialog: true));
+                fullscreenDialog: false));
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  )
-                ]),
-            clipBehavior: Clip.hardEdge,
-            child: Image.network(thumb, headers: const {
-              "User-Agent":
-                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-            }),
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(thumb, headers: const {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              }),
+            ),
           ),
           const SizedBox(
             height: 10,
